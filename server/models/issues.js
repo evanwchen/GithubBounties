4c17d598e04fd8fe72b904bb720d17a1180de15c
Evan Chen <evanwchen@gmail.com>
2016-03-05 13:49:56 -0800

    Fetch userIssues for Profile page

--- server/models/issues.js
+++ server/models/issues.js
@@ -13,61 +13,56 @@ Issues.prototype.getIssues = function () {
   
   if (this._issues.length === 0 ||
    hoursSinceLastFetch > 1) {
-    return db.raw(`select i.*, r.language, r.id as repo_id 
-            from issues i 
-            left join repos r on i.org_name=r.org_name and i.repo_name=r.name 
-            order by created_at desc;`)
-            .then((results) => {
-              results[0].forEach((issue) => {
-                issue.labels = JSON.parse(issue.labels);
-              });
-              this._issues = results[0];
-              this._lastUpdateDate = new Date();
-              return this._issues;
-            })
-            .catch(console.log);
+    return db.raw(`select i.*
+      , r.language
+      , r.id as repo_id 
+      from issues i 
+      left join repos r on i.org_name=r.org_name and i.repo_name=r.name 
+      order by created_at desc;`)
+      .then((results) => {
+        results[0].forEach((issue) => {
+          issue.labels = JSON.parse(issue.labels);
+        });
+        this._issues = results[0];
+        this._lastUpdateDate = new Date();
+        return this._issues;
+      })
+      .catch(console.log);
   } else {
     return new Promise((resolve) => resolve(this._issues));
   }
 };
 
 Issues.prototype.getBounties = function () {
-  // var self = this;
-  // var hoursSinceLastFetch = new DateDiff(new Date(), this._lastUpdateDate).hours();
-  
-  // if (this._issues.length === 0 ||
-  //  hoursSinceLastFetch > 1) {
-  return db.raw(`select i.*, r.language, r.id as repo_id 
-          from bountyissues i 
-          left join repos r on i.org_name=r.org_name and i.repo_name=r.name 
-          order by created_at desc;`)
-          .then((results) => {
-            // results[0].forEach((issue) => {
-            //   issue.labels = JSON.parse(issue.labels);
-            // });
-            // // this._issues = results[0];
-            // console.log('results after forEach', results[0]);
-            return results[0];
-            // this._lastUpdateDate = new Date();
-            // return this._issues;
-          })
-          .catch(console.log);
-  // } else {
-  //   return new Promise((resolve) => resolve(this._issues));
-  // }
+  return db.raw(`select b.*
+    , r.language
+    , r.id as repo_id 
+    from bountyissues b 
+    left join repos r on b.org_name=r.org_name and b.repo_name=r.name 
+    where b.bounty_paid=false
+    order by created_at desc;`)
+    .then((results) => {
+      return results[0];
+    })
+    .catch(console.log);
 };
 
-Issues.prototype.getUserIssues = function (username) {
-
-  // Needs different query
-  return db.raw(`select i.*, r.language, r.id as repo_id 
-          from bountyissues i 
-          left join repos r on i.org_name=r.org_name and i.repo_name=r.name 
-          order by created_at desc;`)
-          .then((results) => {
-            return results[0];
-          })
-          .catch(console.log);
+Issues.prototype.getUserIssues = function (user_id) {
+  return db.raw(`select i.internal_id as internal_id
+    , b.repo_name as repo_name
+    , b.org_name as org_name
+    , b.title as title
+    , b.html_url as html_url
+    , b.bounty_price as bounty_price
+    , b.bitcoin_amount as bitcoin_amount
+    from issuesUsers i, bountyIssues b
+    where i.issue_id=b.id
+    and i.user_id=${user_id}
+    and b.bounty_paid=false;`)
+    .then((results) => {
+      return results[0];
+    })
+    .catch(console.log);
 };
 
 module.exports = Issues;
